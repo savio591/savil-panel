5a1ab86f6fc685b6db910c136f2d7025b8bfb6a0
savio591 <savio591@hotmail.com>
2021-01-08 20:12:47 -0300

    loginServices

--- frontend/src/components/header.tsx
+++ frontend/src/components/header.tsx
@@ -1,4 +1,7 @@
 import React from 'react'
+
+import { IsLoginPage, MessageLoginMgr } from "../services/loginServices";
+
 import '../styles/header.css';
 
 // Cabeçalho do site
@@ -9,37 +12,19 @@ interface HeaderProps {
     username?: string;
     notLoggedText: string;
     loggedText: string;
-    url ?: string;
+    url?: string;
 }
 
-// Tipagem das mensagens de login do cabeçalho
-interface MessageLoginTypes {
-    isLogged: boolean,
-    notLoggedText: string,
-    loggedText: string
-}
-
-// Função que entrega a mensagem de login adequada
-function MessageLoginMgr({ isLogged, loggedText, notLoggedText }: MessageLoginTypes) {
-    if (isLogged) {
-        loginText = loggedText
-        return loginText
-    }
-    else {
-        var loginText = notLoggedText
-        return loginText
-    }
-}
 
 function Header(props: HeaderProps) {
     const name = props.name;
-    const url = 
+
 
     const loginText = MessageLoginMgr({
         isLogged: false,
         loggedText: props.loggedText,
         notLoggedText: props.notLoggedText
-    });
+    })
 
     return (
         <>
@@ -47,7 +32,7 @@ function Header(props: HeaderProps) {
                 <h1 className="header_logo">{name}</h1>
                 <nav className="header_navbar">
                     <div className="admin_button">
-                        <a hidden={true} href="./login">
+                        <a hidden={IsLoginPage()} href="./login">
                             <h3>{loginText}</h3>
                         </a>
 
---
+++ frontend/src/services/loginServices.ts
@@ -0,0 +1,33 @@
+
+
+// Tipagem das mensagens de login do cabeçalho
+interface MessageLoginTypes {
+    isLogged: boolean,
+    notLoggedText: string,
+    loggedText: string
+}
+
+// Função que entrega a mensagem de login adequada
+export function MessageLoginMgr({ isLogged, loggedText, notLoggedText }: MessageLoginTypes) {
+    if (isLogged) {
+        loginText = loggedText
+        return loginText
+    }
+    else {
+        var loginText = notLoggedText
+        return loginText
+    }
+}
+
+export function IsLoginPage() {
+    const url = document.URL
+
+    if (url == "http://localhost:9000/login/") {
+        const isLoginPage: boolean = true
+        return isLoginPage
+    }
+    else {
+        const isLoginPage = false
+        return isLoginPage
+    }
+}
