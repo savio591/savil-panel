6770337004f32f3e042d481b3d6be5dc05584531
savio591 <savio591@hotmail.com>
2021-01-10 20:10:47 +0000

    Deploy https://savilexperiments.com.br//savil-panel/frontend/docs

--- frontend/src/services/api.ts
+++ frontend/src/services/api.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: 'https://3333-e561d1af-5ca4-4d8b-ae8b-7cb2c0a83fa5.ws-us03.gitpod.io'
+    baseURL: 'http://localhost:3333'
 });
 
 export default api
