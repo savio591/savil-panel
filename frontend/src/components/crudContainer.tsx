d23b10af54dae79b6cd4b72b537133ffe0e7d8fc
savio591 <savio591@hotmail.com>
2021-01-13 23:53:39 -0300

    Corpo frontend do CRUD do produto

--- frontend/src/components/crudContainer.tsx
+++ frontend/src/components/crudContainer.tsx
@@ -6,7 +6,24 @@ import '../styles/crudContainer.css'
 function CrudContainer() {
     return (
         <>
-        <section>Teste dashboad</section>
+        <section>
+            <h3>Produtos cadastrados:</h3>
+            <div className="productsCRUD">
+                <div className="productCRUD">
+                    <p>Nome do produto</p>
+                    <div><p>Editar</p></div>
+                    <div><p>Remover</p></div>
+                    <div className="productEdit">
+                        <p>Nome do produto</p>
+                        <p>Descrição do produto</p>
+                        <p>Preço</p>
+                        <p>Categoria</p>
+                        <button className="addProductButton">Salvar</button>
+                    </div>
+                </div>
+                <button className="addProductButton">Adicionar produto</button>
+            </div>
+        </section>
         </>
     )
 }
